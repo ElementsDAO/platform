ad224a477a9d41321897baf4b051dd6366507688
huhn511 <sehe89@gmail.com>
2022-04-09 23:36:48 +0200

    removed next/image

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
@@ -27,30 +26,11 @@ const Layout = ({ children, home }): any => (
     </Head>
     <header className={styles.header}>
       {home ? (
-        <>
-          <Image
-            priority
-            src='/images/profile.jpg'
-            className={utilStyles.borderCircle}
-            height={144}
-            width={144}
-            alt={name}
-          />
-          <h1 className={utilStyles.heading2Xl}>{name}</h1>
-        </>
+        <h1 className={utilStyles.heading2Xl}>{name}</h1>
       ) : (
         <>
           <Link href='/'>
-            <div>
-              <Image
-                priority
-                src='/images/profile.jpg'
-                className={utilStyles.borderCircle}
-                height={108}
-                width={108}
-                alt={name}
-              />
-            </div>
+            <div />
           </Link>
           <h2 className={utilStyles.headingLg}>
             <Link href='/'>
