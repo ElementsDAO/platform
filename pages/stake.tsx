f3e5e87a3057312ea4038ebbff395d364409b8dd
huhn511 <sehe89@gmail.com>
2022-05-07 11:20:30 +0200

    add NFT staking

--- pages/stake.tsx
+++ pages/stake.tsx
@@ -6,8 +6,10 @@ import Web3 from 'web3'
 import {
   Box,
   Button,
-  FormControl,
-  Input,
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
   InputLabel,
   Typography,
 } from '@iotabots/components'
@@ -16,10 +18,14 @@ import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 
-import { ELEMENTS_ADDRESS } from '../config'
+import { ELEMENTS_ADDRESS, NFT_ADDRESS, NFT_STAKE_ADDRESS } from '../config'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ELEMENTS_ABI = require('../contracts/elements.json')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const NFT_STAKE_ABI = require('../contracts/nft_stake.json')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const NFT_ABI = require('../contracts/nft.json')
 
 const DepositInfo = {
   stake: 0,
@@ -30,17 +36,39 @@ const Stake = (): any => {
 
   const { account, library, chainId } = useWeb3React()
   const [contract, setContract] = React.useState(undefined)
+  const [nfts, setNfts] = React.useState(undefined)
+  const [contractNftStake, setContractNftStake] = React.useState(undefined)
+  const [contractNft, setContractNft] = React.useState(undefined)
   const [balance, setBalance] = React.useState(undefined)
+  const [poolBalance, setPoolBalance] = React.useState(undefined)
   const [depositInfo, setDepositInfo] = React.useState(DepositInfo)
 
   const init = async function (_account, _library): Promise<any> {
     const web3 = new Web3(_library.provider)
     const tempContract = new web3.eth.Contract(ELEMENTS_ABI, ELEMENTS_ADDRESS)
+    const tempContractNftStake = new web3.eth.Contract(
+      NFT_STAKE_ABI,
+      NFT_STAKE_ADDRESS
+    )
+    const tempContractNft = new web3.eth.Contract(NFT_ABI, NFT_ADDRESS)
 
     let data = await tempContract.methods.balanceOf(_account).call()
     setContract(tempContract)
+    setContractNftStake(tempContractNftStake)
+    setContractNft(tempContractNft)
     console.log('balance', data)
     setBalance(data)
+    data = await tempContractNft.methods.walletOfOwner(_account).call()
+    console.log('nfts', data)
+    setNfts(data)
+    data = await tempContractNftStake.methods.getStakeContractBalance().call()
+    console.log('poolBalance', data)
+    setPoolBalance(data)
+    data = await tempContractNftStake.methods.receipt(1).call()
+    console.log('receipt', data)
+    data = await tempContractNftStake.methods.getCurrentStakeEarned(1).call()
+    console.log('getCurrentStakeEarned', data)
+
     data = await tempContract.methods.getDepositInfo(_account).call()
     console.log('getDepositInfo', data)
     setDepositInfo({
@@ -66,6 +94,35 @@ const Stake = (): any => {
     }
   }
 
+  const stakeNft = async function (): Promise<any> {
+    try {
+      const amount = balance
+      const res = await contractNft.methods
+        .approve(NFT_STAKE_ADDRESS, 1)
+        .send({ from: account })
+      console.log('approval', res)
+      const data = await contractNftStake.methods
+        .stakeNFT([1])
+        .send({ from: account })
+      console.log('data', data)
+      router.reload()
+    } catch (error) {
+      console.log('error', error)
+    }
+  }
+
+  const unstakeNft = async function (): Promise<any> {
+    try {
+      const data = await contractNftStake.methods
+        .unStakeNFT(1)
+        .send({ from: account })
+      console.log('data', data)
+      router.reload()
+    } catch (error) {
+      console.log('error', error)
+    }
+  }
+
   React.useEffect(() => {
     if (!!account && !!library) {
       init(account, library)
@@ -76,22 +133,69 @@ const Stake = (): any => {
   return (
     <Layout home>
       <Head>
-        <title>Faucet</title>
+        <title>Stake</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <Typography variant='body2' color='text.primary'>
-          Here you can get stake your LMNT tokens to earn some rewards.
+        <Typography variant='h2' color='text.primary'>
+          Stake ELE Tokens.
         </Typography>
         <Typography variant='body2' color='text.primary'>
-          Balance: {balance} LMNT
+          Here you can get stake your ELE tokens to earn some rewards.
         </Typography>
         <Typography variant='body2' color='text.primary'>
-          Staked LMNT: {depositInfo.stake}
+          Balance: {balance} ELE
+        </Typography>
+        <Typography variant='body2' color='text.primary'>
+          Staked ELE: {depositInfo.stake}
         </Typography>
         <Typography variant='body2' color='text.primary'>
           Rewards: {depositInfo.rewards}
         </Typography>
         <Button onClick={() => stake()}>Stake</Button>
+      </section>
+      <section className={utilStyles.headingMd}>
+        <Typography variant='h2' color='text.primary'>
+          Stake Elementary Energy NFTs.
+        </Typography>
+        <Typography gutterBottom variant='h6'>
+          poolBalance:{poolBalance}
+        </Typography>
+        {nfts?.length === 0 ? (
+          <Typography gutterBottom variant='h6'>
+            You don&apos;t own any Elementary Energy NFTs yet :(
+          </Typography>
+        ) : (
+          <Grid container spacing={6}>
+            {nfts?.flatMap((bot) => (
+              <Grid item key={bot} xs={2} sm={3} md={4}>
+                <Link href={`https://elementary.energy/nfts/${bot}`}>
+                  <Card
+                    sx={{
+                      minWidth: '100%',
+                      cursor: 'pointer',
+                      bgcolor: 'rgba(0,0,0,0.5)',
+                    }}
+                  >
+                    <CardMedia
+                      height='100%'
+                      width='100%'
+                      component='img'
+                      image={`https://elementary.energy/nfts/${bot}`}
+                      alt='NFT'
+                    />
+                    <CardContent sx={{ p: 4, pb: '12px !important' }}>
+                      <Typography variant='h6' mb={0}>
+                        {`Elementary Energy NFT #${bot}`}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Link>
+              </Grid>
+            ))}
+          </Grid>
+        )}
+        <Button onClick={() => stakeNft()}>Stake NFT</Button>
+        <Button onClick={() => unstakeNft()}>UnStake NFT</Button>
       </section>
     </Layout>
   )
