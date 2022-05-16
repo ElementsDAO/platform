943a07feb50ff78cecca8716d118345181788e1f
huhn511 <sehe89@gmail.com>
2022-05-17 01:58:12 +0200

    fix identity

--- src/components/Navigation/IdentityMenu.tsx
+++ src/components/Navigation/IdentityMenu.tsx
@@ -5,62 +5,22 @@ import { Web3Provider } from '@ethersproject/providers'
 import { useWeb3React } from '@web3-react/core'
 
 import { ConnectButton } from '../web3/ConnectButton'
-import ProfileImage from './ProfileImage'
-
-interface WhitelistItem {
-  id: string
-  address: string
-}
 
 export const IdentityMenu: React.FC<BoxProps> = (props) => {
   const { account } = useWeb3React<Web3Provider>()
-  const [bots, setBots] = React.useState<number[]>([])
   const { onClick } = props
+  const truncateRegex = /^(0x[a-zA-Z0-9]{3})[a-zA-Z0-9]+([a-zA-Z0-9]{3})$/
 
-  const load = async (): Promise<boolean> => {
-    const URL =
-      'https://raw.githubusercontent.com/iotabots/save-the-bots/main/all.txt'
-
-    const res = await fetch(URL)
-    const data = await res.text()
-
-    const airdropAddresses: WhitelistItem[] = []
-
-    if (!data) {
-      return false
-    }
-
-    const array = data.split('\n')
-
-    for (let index = 0; index < array.length - 1; index += 1) {
-      const botData = array[index].split(':')
-      const obj = {
-        id: botData[0],
-        address: botData[1],
-      }
-      airdropAddresses.push(obj)
-    }
-
-    const iotabots: number[] = []
-    airdropAddresses.forEach((obj) => {
-      if (obj.address === account) {
-        iotabots.push(Number(obj.id))
-      }
-    })
-    setBots(iotabots)
-    return true
+  const truncateEthAddress = (addr: string): string => {
+    const match = addr.match(truncateRegex)
+    if (!match) return addr
+    return `${match[1]}…${match[2]}`
   }
-
-  React.useEffect(() => {
-    load()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [account])
-
   return (
     <Box sx={{ display: { xs: 'none', md: 'block' } }}>
       {account ? (
         <Box sx={{ cursor: 'pointer' }} onClick={onClick}>
-          <ProfileImage id={bots[0]} />
+          {truncateEthAddress(account)}
         </Box>
       ) : (
         <ConnectButton />
